c7d5f61b9f5cc78a72914457c751f2680166f64b
lili <15758064+llliiyyuuu@user.noreply.gitee.com>
2025-05-03 15:47:38 +0800

    Initial commit

---
+++ src/views/components/echarts/world.vue
@@ -0,0 +1,148 @@
+<script setup>
+import world from 'echarts/map/json/world.json'
+import setting from '@/setting.js'
+import {adjustColorOpacity} from '@/utils/util.common.js'
+
+const state = reactive({
+  mapCityOption: {
+    name: 'world',
+    geoData: world
+  },
+  chartOptions: {
+    title: {
+      text: "世界地图",
+      top: "10px",
+      left: "center",
+      textStyle: {
+        color: setting.theme.color,
+      },
+    },
+    visualMap: [
+      {
+        min: 10,
+        max: 1000,
+        left: 'left',
+        top: 'bottom',
+        text: ['高', '低'],
+        calculable: false,
+        orient: 'horizontal',
+        inRange: {
+          color: [adjustColorOpacity(setting.theme.color, 20), setting.theme.color],
+          symbolSize: [30, 100],
+        },
+      },
+    ],
+    geo: {
+      map: 'world',
+      show: false,
+    },
+    tooltip: {
+      trigger: 'item',
+      backgroundColor: 'rgba(0, 0, 0, .6)',
+      textStyle: {
+        color: '#fff',
+        fontSize: 12,
+      },
+    },
+    series: [
+      {
+        type: 'map',
+        map: 'world',
+        label: {
+          show: false,
+          color: '#a7a7a7',
+          fontSize: 13,
+        },
+        itemStyle: {
+          areaColor: adjustColorOpacity(setting.theme.color, 10),
+          borderColor: adjustColorOpacity(setting.theme.color, 80),
+          emphasis: {
+            areaColor: setting.theme.color,
+          }
+        },
+        data: [],
+      },
+      // 添加标记点的series
+      {
+        type: 'effectScatter', // 可以使用scatter或effectScatter
+        coordinateSystem: 'geo',
+        data: [
+          {
+            name: '🧭北京·北京',
+            symbol: 'circle', // 标记点的图形样式，可以是圆圈：'circle'、气泡：'pin'、方块：'rect'等
+            value: [116.2529, 39.546694], // 数值可以影响标记点大小，例如 [经度, 纬度, 100]
+          }
+          // 可以添加多个标记点
+        ],
+        rippleEffect: {
+          brushType: 'stroke'
+        },
+        symbolSize: function (val) {
+          return 10; // 设置标记点的大小为10
+        },
+        hoverAnimation: true,
+        itemStyle: {
+          normal: {
+            color: 'yellow',
+            shadowBlur: 20,
+            shadowColor: 'yellow'
+          }
+        },
+        label: {
+          show: true,
+          color: setting.theme.color,
+          distance: 16,
+          fontSize: 15,
+          fontWeight: 600,
+          position: 'top',
+          formatter: '{b}',
+        },
+      }
+    ],
+  },
+
+
+})
+const countries = [
+  'China', 'United States', 'India', 'Indonesia', 'Pakistan', 'Brazil', 'Nigeria', 'Bangladesh', 'Russia', 'Mexico',
+  'Japan', 'Ethiopia', 'Philippines', 'Egypt', 'Vietnam', 'DR Congo', 'Turkey', 'Iran', 'Germany', 'Thailand',
+  'United Kingdom', 'France', 'Italy', 'Tanzania', 'South Africa', 'Myanmar', 'Kenya', 'South Korea', 'Colombia', 'Spain',
+  'Argentina', 'Australia', 'Canada', 'Chile', 'Cuba', 'Denmark', 'Finland', 'Greece', 'Iceland', 'Ireland',
+  'Israel', 'Jamaica', 'Jordan', 'Kazakhstan', 'Malaysia', 'Morocco', 'Netherlands', 'New Zealand', 'Norway', 'Peru',
+  'Poland', 'Portugal', 'Saudi Arabia', 'Singapore', 'Sweden', 'Switzerland', 'Tunisia', 'Ukraine', 'United Arab Emirates', 'Uruguay',
+  'Austria', 'Belgium', 'Bulgaria', 'Czech Republic', 'Ecuador', 'Hungary', 'Iraq', 'Lebanon', 'Libya', 'Lithuania'
+];
+
+
+const generateData = () => {
+  let tempMapData = []
+  for (let i = 0; i < 70; i++) {
+    const country = {
+      name: countries[i],
+      value: Math.round(Math.random() * 1000),
+      tipData: [Math.round(Math.random() * 1000), Math.round(Math.random() * 1000)],
+    };
+    tempMapData.push(country)
+
+  }
+  state.chartOptions.series[0].data = tempMapData;
+}
+generateData()
+
+onMounted(() => {
+  setInterval(() => {
+    generateData()
+  }, 3000)
+})
+
+</script>
+
+<template>
+  <section>
+    <ZyEcharts :mapCityOption="state.mapCityOption" height="calc(100vh - 130px)" :chartOptions="state.chartOptions"/>
+  </section>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,75 @@
+<template>
+  <section class="home-box">
+    <div class="side-logo">
+      <transition enter-active-class="animate__animated animate__fadeInLeft">
+        <span class="logo-title">零信任信息化平台</span>
+      </transition>
+    </div>
+  
+    
+    <div style="margin-top: 30px">
+      <el-timeline style="max-width: 600px">
+        <el-timeline-item
+            v-for="(activity, index) in activities"
+            :key="index"
+            :icon="activity.icon"
+            :type="activity.type"
+            :color="activity.color"
+            :size="activity.size"
+            :hollow="activity.hollow"
+            :timestamp="activity.timestamp"
+        >
+          {{ activity.content }}
+        </el-timeline-item>
+      </el-timeline>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import { MoreFilled } from '@element-plus/icons-vue'
+import setting from '@/setting.js'
+const activities = [
+  {
+    content: '完善细节',
+    timestamp: '2024-09-05 00:00',
+    type: 'primary',
+  },
+  {
+    content: '基础布局完成',
+    timestamp: '2024-08-28 14:30',
+    type: 'primary',
+    hollow: true,
+  },
+  {
+    content: '项目初始化搭建',
+    timestamp: '2024-07-15',
+  },
+]
+</script>
+
+
+<style lang="scss" scoped>
+.home-box {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  .info-box {
+    font-size: 15px;
+    color: #6c6c6c;
+    text-align: center;
+    line-height: 1.8;
+  }
+
+}
+.side-logo {
+  height: $navbar-height;
+  line-height: $navbar-height;
+  font-weight: bold;
+  font-size: 50px;
+  font-family: "Courier New";
+  color: var(--el-color-primary);
+  margin: 15px;
+}
+</style>
